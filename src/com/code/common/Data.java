b08c60af570859d0abd31ca59c7c072d9c988021
jintkai <710307018@qq.com>
2014-07-02 15:17:09 +0800

    初始化build.xml

---
+++ src/com/code/common/Data.java
@@ -0,0 +1,8 @@
+package com.code.common;
+
+/**
+ * Created by jinkai on 02/07/2014.
+ */
+public class Data {
+    public static String browserType="IE";
+}
---
+++ src/com/code/common/Tools.java
@@ -0,0 +1,7 @@
+package com.code.common;
+
+/**
+ * Created by jinkai on 02/07/2014.
+ */
+public class Tools {
+}
