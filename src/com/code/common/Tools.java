05629a4e0359f56c2a67f77f2fffdc0d500f6eff
jintkai <710307018@qq.com>
2014-07-03 11:23:02 +0800

    封装函数

--- src/com/code/common/Tools.java
+++ src/com/code/common/Tools.java
@@ -1,11 +1,40 @@
 package com.code.common;
 
+import junit.framework.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 /**
  * Created by jinkai on 02/07/2014.
  */
 public class Tools {
-    public Tools()
+
+    WebDriver driver= TestCase.eventDriver;
+
+    public boolean isElementExist(By by)
     {
-        System.out.println("a");
+        try {
+            driver.findElement(by);
+            return true;
+        }catch(NoSuchElementException e)
+        {
+            return false;
+        }
+    }
+
+    public WebElement findBy(By by)
+    {
+        WebElement ele=null;
+        if (isElementExist(by))
+            ele=driver.findElement(by);
+        return ele;
+    }
+
+
+    public void sendKeys(WebElement ele)
+    {
+
     }
 }
